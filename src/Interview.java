4001e0fae7f212fab1dffbc19c0119da014c8e80
esma17 <milkysf@gmail.com>
2021-01-18 13:15:00 -0600

    Created game for interview

---
+++ src/Interview.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Interview {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<String> codeBreakers = new ArrayList<>(Arrays.asList("Adnan", "Mayasa", "Dean", "Inci", "Jamila", "Aiperi", "Jair", "Esma", "Jeren", "Hasanjan", "Seyfi", "Haben"));
+
+        ArrayList<String> questions = new ArrayList<>(Arrays.asList("Difference between Thread.sleep and implicit wait time in selenium",
+                "Example for severity and priority", "How would you handle exception",
+                "What is the difference between findElement and FindElements",
+                "What are locators in Selenium", "What is abstraction in java",
+                "What is Adhoc testing", "Explain the difference between method overriding and method overloading",
+                "Difference between final and finally keywords", "Difference between Arrays and ArrayList",
+                "What is difference between Verification and Validation",
+                "Please give examples for high severity low priority and low severity high priority defect",
+                "What is the difference between re testing and regression testing", "How to find all links in a webpage"));
+        System.out.println(questions.get(rnd.nextInt(questions.size())) + "===>" + codeBreakers.get(rnd.nextInt(codeBreakers.size())));
+
+    }
+}
